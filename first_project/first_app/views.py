c6b0b8d392ff6ac708d7de19459dab8f3667eca0
miguelpengson <miguelpengson@gmail.com>
2022-06-07 16:59:02 +0800

    Created static folder/file, variable templating

--- first_project/first_app/views.py
+++ first_project/first_app/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    my_dict = {'insert_content': "Helllo I am form firstapp views!"}
+    return render(request, 'first_app/index.html', context=my_dict)
