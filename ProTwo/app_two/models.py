fc406db5c76f3d08e146316abca3a62f8b5c1ddd
miguelpengson <miguelpengson@gmail.com>
2022-06-09 20:16:50 +0800

    Population script, template tagging users.html

--- ProTwo/app_two/models.py
+++ ProTwo/app_two/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class User(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
-    email = models.EmailField(max_length=64, unique=True, default='name@email.com')
+    email = models.EmailField(max_length=64, unique=True)# default='name@email.com'
 
     def __str__(self):
         return self.first_name
--- ProTwo/populate_app_two.py
+++ ProTwo/populate_app_two.py
@@ -13,4 +13,17 @@ fakegen = Faker()
 
 def populate(N=5):
     for entry in range(N):
-        fake_name =
+        fake_name = fakegen.name().split()
+        fake_first_name = fake_name[0]
+        fake_last_name = fake_name[1]
+        fake_email = fakegen.email()
+        # New entry
+        user = User.objects.get_or_create(
+            first_name = fake_first_name,
+            last_name = fake_last_name,
+            email = fake_email)[0]
+
+if __name__ == '__main__':
+    print('Populating Databases')
+    populate()
+    print('Population Complete!')
