f6096c86bc72bf9efc00a04268d0e24aa97f21f4
miguelpengson <miguelpengson@gmail.com>
2022-06-09 10:49:03 +0800

    Import models to views, edited views and index template to show fake data

--- first_project/first_app/views.py
+++ first_project/first_app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from first_app.models import Topic, Webpage, AccessRecord
 
 def index(request):
-    my_dict = {'insert_content': "Helllo I am form firstapp views!"}
-    return render(request, 'first_app/index.html', context=my_dict)
+    webpages_list = AccessRecord.objects.order_by('date')
+    date_dict = {'access_records': webpages_list}
+    return render(request, 'first_app/index.html', context=date_dict)
