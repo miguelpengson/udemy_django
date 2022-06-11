9e5b105129f477e48471539ff6616fb9bb739884
miguelpengson <miguelpengson@gmail.com>
2022-06-11 18:45:41 +0800

    Validation of forms

--- basic_forms/basic_app/views.py
+++ basic_forms/basic_app/views.py
@@ -7,8 +7,12 @@ def index(request):
 def form_name_view(request):
     form = forms.FormName()
     if request.method =='POST':
-        
-
+        form = forms.FormName(request.POST)
+        if form.is_valid():
+            print("Validation Success!")
+            print("NAME: " + form.cleaned_data['name'])
+            print("EMAIL: " + form.cleaned_data['email'])
+            print("TEXT: " + form.cleaned_data['text'])
 
 
     return render(request, 'basic_app/form_page.html', {'form':form})
