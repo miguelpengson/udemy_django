66b93dc0551c4eacc35a4916d23d07e392098b38
miguelpengson <miguelpengson@gmail.com>
2022-06-15 17:11:25 +0800

    fix html files and urls

--- learning_users/basic_app/views.py
+++ learning_users/basic_app/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+
+
+    return render(request, 'basic_app/index.html')
+
+
+def register(request):
+    registered = False
+    if request.method == "POST":
+        user_form
+    return render(request, 'basic_app/register.html')
