fb6862e931034ccebc6db156f34a602a31dc48a0
miguelpengson <miguelpengson@gmail.com>
2022-06-25 18:33:05 +0800

    getting started with login views and html

--- learning_users/basic_app/views.py
+++ learning_users/basic_app/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
 from .forms import UserForm, UserProfileInfoForm
 
+
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponse, HttpResponseRedirect, HtttpResponse
+# old before django 4.0 from django.core.urlresolvers imort reverse
+from django.urls import reverse
+from django.contrib.auth.decorators import login_required
+
+
 def index(request):
     return render(request, 'basic_app/index.html')
+
+@login_required
+def user_logout(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('index'))
 
 
 def register(request):
@@ -39,3 +52,25 @@ def register(request):
 
     return render(request, 'basic_app/registeration.html',{
         'user_form':user_form, 'profile_form':profile_form, 'registered':registered})
+
+
+def user_login(request):
+    
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(username=username, password=password)
+
+        if user:
+            if user.is_active:
+                login(request, user)
+                return HttpResponseRedirect(reverse('index'))
+            else:
+                return HtttpResponse("ACCOUNT NOT ACTIVE")
+        else:
+            print('Someone tried to login and failed!')
+            print(f"Username: {'username'} and password: {'password'}")
+            return HttpResponse("invalid login details supplied")
+    else:
+        return render(request, 'basic_app/login.html', {})
